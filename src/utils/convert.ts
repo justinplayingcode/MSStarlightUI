8abc5e431c33a6cdbd21fba8d7eeb628d3821fba
Justin small hands <phamduythang00@gmail.com>
2023-04-29 23:55:20 +0700

    move utils func to class

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -1,5 +1,16 @@
-export const getAge = (dateOfBirth: Date) => {
-    const year = dateOfBirth.getFullYear();
-    const current = new Date().getFullYear();
-    return current - year;
+export default class Convert {
+    public static shortenString = (t: string, n: number):string => {
+            if(t.length > n) {
+                return `${t.slice(0,n)}...`;
+        } else {
+                return t;
+        }
+    }
+
+    public static getAge = (dateOfBirth: Date) => {
+        const year = dateOfBirth.getFullYear();
+        const current = new Date().getFullYear();
+        return (current - year).toString();
+    }
+
 }
