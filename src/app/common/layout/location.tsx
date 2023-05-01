cc6046b0598e5bdd08c6751c17e7dfde145daadc
Dự Nguyễn Trọng <nguyentrongdupl@gmail.com>
2023-05-01 15:31:37 +0700

    breadcrumb fix

--- src/app/common/layout/location.tsx
+++ src/app/common/layout/location.tsx
@@ -1,38 +1,50 @@
-import { Breadcrumb, IBreadcrumbItem, Stack } from '@fluentui/react';
+import { Breadcrumb, DefaultButton, IBreadcrumbItem, Stack } from '@fluentui/react';
 import * as React from 'react'
-import { useLocation } from 'react-router-dom'
-import { getRouteItemByKey, getRouteItemByUrl } from './route.type';
+import { useLocation, useNavigate } from 'react-router-dom'
+import { getRouteItemByKey, getRouteItemByUrl, routeMapping } from './route.type';
 import './location.scss'
 
 export const Location = () => {
     const { pathname } = useLocation();
+    const navigate = useNavigate();
 
     const assembleParentItem = (key: string) => {
         const routeItem = getRouteItemByKey(key);
-        const item: IBreadcrumbItem ={
-            key: routeItem.key,
-            text: routeItem.text
+        console.log(routeItem);
+        
+        const item: IBreadcrumbItem = {
+            text:'',
+            key:''
         };
+        if (routeItem.key !== 'route-home'){
+            item.key = routeItem.key;
+            item.text = routeItem.text
+        };
+        
         if (routeItem.url){
-            item.href = `#${routeItem.url}`;
+            item.onClick = () => {
+                navigate(routeItem.url)
+            }
         } else {
             item.style = { textDecoration: "unset"};
-        }
+        }        
         return item;
     }
 
     const assembleBreadItem = React.useCallback(() => {
-        const routeItem = getRouteItemByUrl(pathname);
+        const routeItem = getRouteItemByUrl(pathname);        
         const items: IBreadcrumbItem[] = [];
         if( routeItem?.parentKeys?.length){
             routeItem.parentKeys.forEach((key) => {
                 items.push(assembleParentItem(key))
             });
         }
-        items.push({
-            key: routeItem.key,
-            text: routeItem.text
-        });
+        if(routeItem.key !== 'route-home'){
+            items.push({
+                key: routeItem.key,
+                text: routeItem.text
+            });
+        }
         return items;
     },[pathname]);
 
