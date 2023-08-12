e4a2347347a6d57aef75dfceb42ed08f0be3d824
justin.pham <phamduythang00@gmail.com>
2023-08-12 13:46:24 +0700

    fix path

--- src/app/common/stepper/index.tsx
+++ src/app/common/stepper/index.tsx
@@ -1,6 +1,98 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import StepContent from '@mui/material/StepContent';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import "./index.scss"
 
-import VerticalLinearStepper from "./VerticalStepper"
 
-export default {
-  VerticalLinearStepper
+export interface IStep {
+  label: string;
+  content: JSX.Element;
+  disableNextStep: boolean;
+  handleBackStep?: () => void;
+  handleNextStep?: () => void;
+}
+interface IVerticalLinearStepperProps {
+  steps: IStep[]
+  handleSubmit: () => void;
+  handleReset?: () => void;
+}
+
+export default function VerticalLinearStepper({...props}: IVerticalLinearStepperProps) {
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+    !!props.handleReset && props.handleReset();
+  };
+
+  return (
+    <div className='stepper-container'>
+      <Box sx={{ maxWidth: "100%" }}>
+        <Stepper activeStep={activeStep} orientation="vertical">
+          {props.steps.map((step, index) => (
+            <Step key={step.label}>
+              <StepLabel
+                optional={
+                    <Typography variant="caption">{`Bước ${index+1}`}</Typography>
+                }
+              >
+                {step.label}
+              </StepLabel>
+              <StepContent>
+                <Typography>{step.content}</Typography>
+                <Box sx={{ mb: 2 }}>
+                  <div>
+                    <Button
+                      variant="contained"
+                      onClick={() => {
+                        !!step.handleNextStep && step.handleNextStep();
+                        handleNext()
+                      }}
+                      sx={{ mt: 1, mr: 1 }}
+                      disabled={step.disableNextStep}
+                    >
+                      {index === props.steps.length - 1 ? 'Hoàn thành' : 'Tiếp theo'}
+                    </Button>
+                    <Button
+                      disabled={index === 0}
+                      onClick={() => {
+                        !!step.handleBackStep && step.handleBackStep();
+                        handleBack()
+                      }}
+                      sx={{ mt: 1, mr: 1 }}
+                    >
+                      Quay lại
+                    </Button>
+                  </div>
+                </Box>
+              </StepContent>
+            </Step>
+          ))}
+        </Stepper>
+        {activeStep === props.steps.length && (
+          <Paper square elevation={0} sx={{ p: 3 }}>
+            <Typography>Tất cả các bước đã hoàn thành</Typography>
+            <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+              Đặt lại
+            </Button>
+            <Button variant="contained" onClick={props.handleSubmit} sx={{ mt: 1, mr: 1 }}>Lưu</Button>
+          </Paper>
+        )}
+      </Box>
+    </div>
+  );
 }
